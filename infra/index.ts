c09d85ed1a594d8cd2149a9602a42f9e46b2f7a1
JacopoMadaluni <jacopo.madaluni@gmail.com>
2023-09-14 07:48:44 +0100

    AI generated

--- infra/index.ts
+++ infra/index.ts
@@ -1,117 +1,69 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
 import * as azure_native from "@pulumi/azure-native";
-import * as dotenv from "dotenv";
 
-dotenv.config();
+const config = new pulumi.Config();
 
-const env = "main";
+const resourceGroup = new azure_native.resources.ResourceGroup(
+  "resourceGroup",
+  {
+    resourceGroupName: "apiResourceGroupName",
+    location: "West Europe",
+  }
+);
 
-export = async () => {
-  const resourceGroup = new azure.core.ResourceGroup("eft-resource-group", {
-    name: "eft-resource-group",
-    location: "UKSouth",
-  });
-
-  const storageAccount = new azure_native.storage.StorageAccount(
-    "adam-storage-" + env,
-    {
-      kind: "StorageV2",
-      location: "UK South",
-      accountName: "audiogenadamstorage" + env,
-      resourceGroupName: resourceGroup.name,
-      sku: {
-        name: "Standard_LRS",
-      },
+const storageAccount = new azure_native.storage.StorageAccount(
+  "storageAccount",
+  {
+    resourceGroupName: resourceGroup.name,
+    accountName: "storageaccount",
+    kind: "StorageV2",
+    sku: {
+      name: "Standard_LRS",
     },
-    {
-      parent: resourceGroup,
-    }
-  );
+  }
+);
 
-  const container = new azure.storage.Container(
-    "container-" + env,
-    {
-      storageAccountName: storageAccount.name,
-      name: "container",
-      containerAccessType: "private",
+const container = new azure_native.storage.BlobContainer("container", {
+  resourceGroupName: resourceGroup.name,
+  accountName: storageAccount.name,
+  containerName: "container",
+});
+
+const appServicePlan = new azure_native.web.AppServicePlan("appServicePlan", {
+  resourceGroupName: resourceGroup.name,
+  name: "appServicePlan",
+  kind: "Linux",
+  reserved: true,
+  sku: {
+    name: "B1",
+    tier: "Basic",
+  },
+});
+
+const webApp = new azure_native.web.WebApp("webApp", {
+  resourceGroupName: resourceGroup.name,
+  name: "apiAppName",
+  serverFarmId: appServicePlan.id,
+  siteConfig: {
+    alwaysOn: false,
+    nodeVersion: "14-lts",
+    linuxFxVersion: "NODE|14-lts",
+  },
+});
+
+const appSettings = new azure_native.web.WebAppApplicationSettings(
+  "appSettings",
+  {
+    resourceGroupName: resourceGroup.name,
+    name: webApp.name,
+    properties: {
+      STORAGE_CONNECTION_STRING: storageAccount.primaryConnectionString,
+      CONTAINER: container.name,
+      API_KEY: config.requireSecret("API_KEY"),
+      PORT: "3000",
     },
-    {
-      parent: storageAccount,
-    }
-  );
+  }
+);
 
-  const servicePlan = new azure_native.web.AppServicePlan(
-    "eft-service-plan-" + env,
-    {
-      kind: "Linux",
-      reserved: true,
-      location: "UK South",
-      name: "eft-sp-" + env,
-      resourceGroupName: resourceGroup.name,
-      sku: {
-        name: "B1",
-        capacity: 1,
-        size: "B1",
-        tier: "Basic",
-      },
-    },
-    {
-      parent: resourceGroup,
-    }
-  );
-
-  const appService = new azure_native.web.WebApp(
-    "eft-app-service-" + env,
-    {
-      name: "eft-as-" + env,
-      resourceGroupName: resourceGroup.name,
-      serverFarmId: servicePlan.id,
-      siteConfig: {
-        alwaysOn: true,
-        nodeVersion: "16.8.0",
-        linuxFxVersion: "Node|16",
-      },
-    },
-    {
-      parent: servicePlan,
-    }
-  );
-
-  const storageAccountKeys = pulumi
-    .all([storageAccount.name, resourceGroup.name])
-    .apply(async ([accountName, resourceGroupName]) => {
-      return await azure_native.storage.listStorageAccountKeys({
-        accountName,
-        resourceGroupName,
-      });
-    });
-
-  const applicationENV = {
-    STORAGE_CONNECTION_STRING:
-      "DefaultEndpointsProtocol=https;AccountName=" +
-      storageAccount.name +
-      ";AccountKey=" +
-      storageAccountKeys.keys[0].value +
-      ";EndpointSuffix=core.windows.net",
-    CONTAINER: container.name,
-    DEBUG: "true",
-  };
-
-  const applicationSettings = new azure_native.web.WebAppApplicationSettings(
-    "eft-application-settings-" + env,
-    {
-      name: appService.name,
-      resourceGroupName: resourceGroup.name,
-      properties: applicationENV,
-    },
-    {
-      parent: appService,
-    }
-  );
-
-  return {
-    apiAppName: appService.name,
-    apiResourceGroupName: resourceGroup.name,
-  };
-};
+export const apiAppName = webApp.name;
+export const apiResourceGroupName = resourceGroup.name;
