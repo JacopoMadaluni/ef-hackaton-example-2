1a65b1f91b4d42995ea2d379f98e59f75e782aae
JacopoMadaluni <jacopo.madaluni@gmail.com>
2023-09-14 09:17:56 +0100

    AI generated

--- infra/index.ts
+++ infra/index.ts
@@ -1,66 +1,61 @@
+
 import * as pulumi from "@pulumi/pulumi";
 import * as azure_native from "@pulumi/azure-native";
 
 const config = new pulumi.Config();
-const location = "WestEurope";
-
 const resourceGroup = new azure_native.resources.ResourceGroup("efrg", {
-  location: location,
+    location: "WestEurope",
 });
 
 const storageAccount = new azure_native.storage.StorageAccount("efsa", {
-  resourceGroupName: resourceGroup.name,
-  sku: {
-    name: "Standard_LRS",
-  },
-  kind: "StorageV2",
-  location: location,
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: "Standard_LRS",
+    },
+    kind: "StorageV2",
 });
 
 const storageAccountKeys = pulumi
-  .all([storageAccount.name, resourceGroup.name])
-  .apply(async ([accountName, resourceGroupName]) => {
-    return await azure_native.storage.listStorageAccountKeys({
-      accountName,
-      resourceGroupName,
+    .all([storageAccount.name, resourceGroup.name])
+    .apply(async ([accountName, resourceGroupName]) => {
+        return await azure_native.storage.listStorageAccountKeys({
+            accountName,
+            resourceGroupName,
+        });
     });
-  });
 
 const appServicePlan = new azure_native.web.AppServicePlan("efasp", {
-  location: location,
-  resourceGroupName: resourceGroup.name,
-  kind: "Linux",
-  reserved: true,
-  sku: {
-    name: "B1",
-    tier: "Basic",
-  },
+    resourceGroupName: resourceGroup.name,
+    kind: "Linux",
+    reserved: true,
+    sku: {
+        name: "B1",
+        tier: "Basic",
+    },
 });
 
-const app = new azure_native.web.WebApp("efwa", {
-  location: location,
-  resourceGroupName: resourceGroup.name,
-  serverFarmId: appServicePlan.id,
-  siteConfig: {
-    alwaysOn: false,
-    nodeVersion: "14-lts",
-    linuxFxVersion: "NODE|14-lts",
-  },
+const app = new azure_native.web.WebApp("efapp", {
+    resourceGroupName: resourceGroup.name,
+    serverFarmId: appServicePlan.id,
+    siteConfig: {
+        alwaysOn: false,
+        nodeVersion: "14.15.0",
+        linuxFxVersion: "NODE|14.15.0",
+    },
 });
 
-new azure_native.web.WebAppApplicationSettings("efwaas", {
-  name: app.name,
-  resourceGroupName: resourceGroup.name,
-  properties: {
-    STORAGE_CONNECTION_STRING:
-      "DefaultEndpointsProtocol=https;AccountName=" +
-      storageAccount.name +
-      ";AccountKey=" +
-      storageAccountKeys.keys[0].value +
-      ";EndpointSuffix=core.windows.net",
-    CONTAINER: "mycontainer",
-    PORT: "3000",
-  },
+const appSettings = new azure_native.web.WebAppApplicationSettings("efas", {
+    resourceGroupName: resourceGroup.name,
+    name: app.name,
+    properties: {
+        STORAGE_CONNECTION_STRING: "DefaultEndpointsProtocol=https;AccountName=" +
+            storageAccount.name +
+            ";AccountKey=" +
+            storageAccountKeys.keys[0].value +
+            ";EndpointSuffix=core.windows.net",
+        CONTAINER: "mycontainer",
+        PORT: "3000",
+    },
 });
 
 export const apiAppName = app.name;
